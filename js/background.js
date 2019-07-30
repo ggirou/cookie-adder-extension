bac0d91209c32b5cec3c93df16df9b201bab9ca1
Guillaume Girou <porude-git@yahoo.fr>
2019-07-30 11:58:42 +0200

    First commit

---
+++ js/background.js
@@ -0,0 +1,51 @@
+/**
+ * Cookie Adder
+ * Copyright (C) 2019 Guillaume Girou
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const defaultOptions = {
+  urls: [],
+  cookie: "",
+};
+var options;
+
+function rewriteCookie(e) {
+  var done = false;
+  e.requestHeaders.forEach(function (header) {
+    if (header.name.toLowerCase() == "cookie") {
+      header.value = options.cookie + + "; " + header.value;
+      done = true;
+    }
+  });
+  if (!done) {
+    e.requestHeaders.push({ name: "Cookie", value: options.cookie });
+  }
+  return { requestHeaders: e.requestHeaders };
+}
+
+async function registerListeners() {
+  options = (await browser.storage.local.get({ options: defaultOptions })).options;
+
+  browser.webRequest.onBeforeSendHeaders.removeListener(rewriteCookie);
+  browser.webRequest.onBeforeSendHeaders.addListener(
+    rewriteCookie,
+    { urls: options.urls },
+    ["blocking", "requestHeaders"]
+  );
+  console.log("Listeners registered", options);
+}
+
+registerListeners();
---
+++ js/options.js
@@ -0,0 +1,66 @@
+/**
+ * Cookie Adder
+ * Copyright (C) 2019 Guillaume Girou
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function () {
+    "use strict";
+
+    var cookieInput = document.querySelector('#cookieInput');
+    var urlsTextarea = document.querySelector('#urls');
+    var saveStatusElement = document.getElementById('saveStatus');
+
+    function showSavedNotification() {
+        // Update status to let user know options were saved.
+        saveStatusElement.style.visibility = "";
+        setTimeout(function () {
+            saveStatusElement.style.visibility = "hidden";
+        }, 2000);
+    };
+
+    function show(options) {
+        cookieInput.value = options.cookie;
+        urlsTextarea.value = options.urls.join("\n");
+    }
+
+    // Saves urls to chrome.storage.local.
+    async function save() {
+        var value = urlsTextarea.value.trim();
+        var newUrls = value ? value.replace(/[\r\n]+/g, "\n").split("\n") : [];
+
+        var options = {
+            urls: newUrls,
+            cookie: cookieInput.value
+        };
+
+        await browser.storage.local.set({ options: options });
+        const backgroundPage = (await browser.runtime.getBackgroundPage());
+        backgroundPage.registerListeners();
+        show(options);
+        showSavedNotification();
+    }
+
+    // Restores the preferences stored in chrome.storage.
+    async function load() {
+        const defaultOptions = (await browser.runtime.getBackgroundPage()).defaultOptions;
+        const options = (await browser.storage.local.get({ options: defaultOptions })).options;
+        show(options);
+        console.log("Options loaded", defaultOptions, options);
+    }
+
+    document.addEventListener('DOMContentLoaded', load);
+    document.querySelector('#saveButton').addEventListener('click', save);
+})();
